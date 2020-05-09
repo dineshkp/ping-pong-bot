86cdeedc1e29b60793fce9d7cf34d99192ef4354
Dinesh Kumar P <git@dineshkp.com>
2020-05-10 00:56:47 +0530

    Add config file to help control a few things from a central point
    Enhance the bot to accept a new command Beep-Boop

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { prefix } = require('./config.json');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -8,11 +9,29 @@ client.once('ready', () => {
 });
 
 client.on('message', (message) => {
-	return message.content === 'ping' ? replyPong(message) : false;
+	// do not proceed if the message does not begin with the prefix.
+	if (!message.content.startsWith(prefix)) return;
+
+	const command = message.content.split(' ')[0];
+
+	switch (command) {
+	case `${prefix}ping`:
+		replyPong(message);
+		break;
+	case `${prefix}beep`:
+		replyBoop(message);
+		break;
+	default:
+		return message.channel.send('I don\'t know how to help you there');
+	}
 });
 
 const replyPong = (message) => {
 	message.channel.send('Pong');
+};
+
+const replyBoop = (message) => {
+	message.channel.send('Boop');
 };
 
 client.login(process.env.DISCORD_TOKEN).then(() => {
